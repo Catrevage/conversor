8bce268958865d5ce1595feedb581ab4b3e2c0ef
Catrevage <damiaovcjr@hotmail.com>
2023-06-30 14:31:29 -0300

    Initial commit

---
+++ src/br/com/conversor/main.java
@@ -0,0 +1,11 @@
+package br.com.conversor;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		System.out.println("teste de commit");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Damião Júnior
+ *
+ */
+module conversor {
+}
